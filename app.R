a8cfacd870352e189d8a49bc93e980ce7c1835d3
prithaxx <105132736+prithaxx@users.noreply.github.com>
2024-10-24 10:52:25 -0500

    Input popup for the histograms

--- app.R
+++ app.R
@@ -7,62 +7,62 @@ data(airquality)
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 
-                # navbar
-                navbarPage(
-                  "Air Quality",
-                  tabPanel("Histograms",
-                           titlePanel("Ozone"),
-                           p(HTML("Daily readings of the following air quality values for May 1, 1973 (a Tuesday) to September 30, 1973. <br>
-      <ul>
-        <li> <b>Ozone:</b> Mean ozone in parts per billion from 1300 to 1500 hours at Roosevelt Island </li>
-        <li> <b>Solar.R</b>: Solar radiation in Langleys in the frequency band 4000–7700 Angstroms from 0800 to 1200 hours at Central Park </li>
-        <li> <b>Wind</b>: Average wind speed in miles per hour at 0700 and 1000 hours at LaGuardia Airport </li>
-        <li> <b>Temp</b>: Maximum daily temperature in degrees Fahrenheit at La Guardia Airport. </li>")),
-                           hr(),
-                           
-                           # Layouts for side-by-side elements
-                           fluidRow(
-                             column(
-                               width = 6,
-                               # Month Chooser: Radio buttons
-                               radioButtons("radio", label = h4("Select a month"),
-                                            choices = list("May" = 5, "June" = 6, "July" = 7, "August" = 8, "September" = 9), 
-                                            selected = 5),
-                             ),
-                             
-                             column(
-                               width = 6,
-                               # Select Constraint
-                               selectInput("select", label = h4("Select constraints"), 
-                                           choices = list("Ozone" = 1, "Solar Radiation" = 2, "Wind" = 3, "Temperature" = 4),
-                                           selected = 1),
-                             )
-                           ),
-                           
-                           hr(),
-                           
-                           # Bins: Slider
-                           sidebarLayout(
-                             sidebarPanel(
-                               sliderInput(inputId = "bins",
-                                           label = "Number of bins:",
-                                           min = 1,
-                                           max = 50,
-                                           value = 30),
-                               
-                               actionButton("save_btn", "Save Histogram"),
-                               hr(),
-                               h4("Saved Histograms"),
-                               uiOutput("saved_plots"),
-                             ),
-                             
-                             # Main panel for displaying outputs ----
-                             mainPanel(
-                               plotOutput(outputId = "distPlot")
-                             )
-                           )
-                  ) # Ozone, tabPanel
-                )
+# navbar
+navbarPage(
+"Air Quality",
+tabPanel("Histograms",
+   titlePanel("Ozone"),
+   p(HTML("Daily readings of the following air quality values for May 1, 1973 (a Tuesday) to September 30, 1973. <br>
+          <ul>
+          <li> <b>Ozone:</b> Mean ozone in parts per billion from 1300 to 1500 hours at Roosevelt Island </li>
+          <li> <b>Solar.R</b>: Solar radiation in Langleys in the frequency band 4000–7700 Angstroms from 0800 to 1200 hours at Central Park </li>
+          <li> <b>Wind</b>: Average wind speed in miles per hour at 0700 and 1000 hours at LaGuardia Airport </li>
+          <li> <b>Temp</b>: Maximum daily temperature in degrees Fahrenheit at La Guardia Airport. </li>")),
+   hr(),
+       
+     # Layouts for side-by-side elements
+     fluidRow(
+       column(
+         width = 6,
+         # Month Chooser: Radio buttons
+         radioButtons("radio", label = h4("Select a month"),
+                      choices = list("May" = 5, "June" = 6, "July" = 7, "August" = 8, "September" = 9), 
+                      selected = 5),
+       ),
+       
+       column(
+         width = 6,
+         # Select Constraint
+         selectInput("select", label = h4("Select constraints"), 
+                     choices = list("Ozone" = 1, "Solar Radiation" = 2, "Wind" = 3, "Temperature" = 4),
+                     selected = 1),
+       )
+     ),
+     
+     hr(),
+     
+     # Bins: Slider
+     sidebarLayout(
+       sidebarPanel(
+         sliderInput(inputId = "bins",
+                     label = "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30),
+         
+         actionButton("save_btn", "Save Histogram"),
+         hr(),
+         h4("Saved Histograms"),
+         uiOutput("saved_plots"),
+       ),
+       
+       # Main panel for displaying outputs ----
+       mainPanel(
+         plotOutput(outputId = "distPlot")
+       )
+     )
+    ) 
+  )
 )
 
 # Define server logic required to draw a histogram ----
@@ -107,7 +107,24 @@ server <- function(input, output, session) {
   
   # Save histogram
   observeEvent(input$save_btn, {
+    # Show a modal to ask for histogram name
+    showModal(modalDialog(
+      title = "Save Histogram",
+      textInput("histogram_name", "Enter a name for the histogram", value = "Histogram name"),
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton("confirm_save", "Save")
+      )
+    ))
+  })
+  
+  # When the user clicks save in the modal
+  observeEvent(input$confirm_save, {
+    removeModal()  # Remove the modal
+    
+    # Save the histogram details
     current_hist <- list(
+      name = input$histogram_name,
       month = input$radio,
       constraint = input$select,
       bins = input$bins
@@ -130,7 +147,7 @@ server <- function(input, output, session) {
     
     buttons <- lapply(1:length(hist_list), function(i) {
       hist_data <- hist_list[[i]]
-      actionButton(paste0("load_hist_", i), paste("Histogram", i))
+      actionButton(paste0("load_hist_", i), hist_data$name)
     })
     
     do.call(tagList, buttons)
@@ -152,4 +169,4 @@ server <- function(input, output, session) {
 }
 
 # Create Shiny app ----
-shinyApp(ui = ui, server = server, enableBookmarking = "url")
+shinyApp(ui = ui, server = server)
